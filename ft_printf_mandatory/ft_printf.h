a1303b25cbe98ccaeac634856c4e8058db5e6eda
matcardo <matcardo@student.42sp.org.br>
2022-06-05 00:00:34 +0200

    Delte makefile because no needs now and change name of ft_printf and create header

---
+++ ft_printf_mandatory/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/04 23:47:30 by matcardo          #+#    #+#             */
+/*   Updated: 2022/06/04 23:50:03 by matcardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+# define FT_PRINTF
+
+# include <stdarg.h>
+# include <unistd.h>
+# include "Libft/libft.h"
+
+int		ft_printf(const char *str, ...);
+
+#endif
