064072671a4133d13ae50ffd373aad2464f0d088
matcardo <matcardo@student.42sp.org.br>
2022-06-08 05:26:38 +0200

    complete working with make

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 23:47:30 by matcardo          #+#    #+#             */
-/*   Updated: 2022/06/08 05:03:43 by matcardo         ###   ########.fr       */
+/*   Updated: 2022/06/08 05:22:08 by matcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		ft_printf(const char *str, ...);
 int		putstr(char *str);
 int		putnbr(int n);
 int		putnbr_u(unsigned int n);
-int		puthex(unsigned int n, int flag);
+int		puthex(unsigned long int n, int flag);
 int		putptr(unsigned long int n);
 
 #endif
--- puthex.c
+++ puthex.c
@@ -6,37 +6,20 @@
 /*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 08:18:49 by matcardo          #+#    #+#             */
-/*   Updated: 2022/06/08 05:07:14 by matcardo         ###   ########.fr       */
+/*   Updated: 2022/06/08 05:23:55 by matcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-unsigned int	count_base_hex(unsigned int n)
+unsigned long int	count_base_hex(unsigned long int n)
 {
 	if (n <= 15)
 		return (1);
-	return ((unsigned int)(count_base_hex(n / 16) * 16));
+	return ((unsigned long int)(count_base_hex(n / 16) * 16));
 }
 
-unsigned long int	count_base_long_hex(unsigned long int n)
-{
-	if (n <= 15)
-		return (1);
-	return ((unsigned long int)(count_base_long_hex(n / 16) * 16));
-}
-
-char	hex(unsigned int n, int isUpper)
-{
-	char	*hexstrlower = "0123456789abcdef";
-	char	*hexstrupper = "0123456789ABCDEF";
-
-	if (isUpper)
-		return(hexstrupper[(unsigned int)n]);
-	return(hexstrlower[(unsigned int)n]);
-}
-
-char	long_hex(unsigned long int n, int isUpper)
+char	hex(unsigned long int n, int isUpper)
 {
 	char	*hexstrlower = "0123456789abcdef";
 	char	*hexstrupper = "0123456789ABCDEF";
@@ -46,10 +29,10 @@ char	long_hex(unsigned long int n, int isUpper)
 	return(hexstrlower[(unsigned long int)n]);
 }
 
-int		puthex(unsigned int n, int flag)
+int		puthex(unsigned long int n, int flag)
 {
-	unsigned int	n_iter;
-	unsigned int	base;
+	unsigned long int	n_iter;
+	unsigned long int	base;
 	int	count;
 
 	count = 0;
@@ -66,31 +49,11 @@ int		puthex(unsigned int n, int flag)
 	return(count);
 }
 
-int		put_long_hex(unsigned long int n, int flag)
-{
-	unsigned long int	n_iter;
-	unsigned long int	base;
-	int	count;
-
-	count = 0;
-	n_iter = n;
-	base = count_base_long_hex(n);
-	while (base >= 1)
-	{
-		n = n_iter / base;
-		ft_putchar_fd(long_hex(n, flag), 1);
-		count++;
-		n_iter -= base * n;
-		base = base / 16;
-	}
-	return(count);
-}
-
 int		putptr(unsigned long int n)
 {
 	int	count;
 	
 	write(1, "0x", 2);
-	count = put_long_hex(n, 0) + 2;
+	count = puthex(n, 0) + 2;
 	return(count);
 }
